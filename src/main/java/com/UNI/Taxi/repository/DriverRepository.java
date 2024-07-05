6188ce13390bcc816bad753b8eeadb52ba5e23fb
asallbabaeii <AsalBabaei2001@gmail.com>
2024-07-05 17:48:10 +0430

    Add Driver Repository Class

---
+++ src/main/java/com/UNI/Taxi/repository/DriverRepository.java
@@ -0,0 +1,22 @@
+package com.UNI.Taxi.repository;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import com.UNI.Taxi.entity.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public interface DriverRepository extends JpaRepository<Driver,Long>
+{
+    List<Driver> findByFamilyContainingIgnoreCase(String keyword);
+
+    @Query("UPDATE Driver t SET t.published = :published WHERE t.id = :id")
+    @Modifying
+    public void updatePublishedStatus(Long id, boolean published);
+}
